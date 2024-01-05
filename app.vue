f2bedf315d30db6b5e038d4f1a81d62adcce6005
brandon <brandoge91@gmail.com>
2024-01-05 15:56:22 +0000

    Update code formatting

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+function copy() {
+    navigator.clipboard.writeText('3.19.484.24')
+}
+</script>
+
+<template>
+    <div class="relative text-center top-1/3">
+        <h1 class="text-white font-bold text-7xl">SKYDNS</h1>
+        <h3 class="text-white font-semibold text-xl">Bringing Skylanders mobile back</h3>
+        <div class="flex justify-center mt-10">
+            <a @click="copy" target="_blank" class="cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                3.19.484.24
+                <Icon name="material-symbols:content-copy-outline" size="24" class="mt-n2" color="black" />
+            </a>
+            </div>
+    </div>
+</template>
