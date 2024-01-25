a09c30ea2c5644f3c08283defe528a9da866fa72
brandoge91 <brandoge91@gmail.com>
2024-01-24 21:39:55 -0500

    Refactor copy function to use clipboard API

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <script setup>
 const buttonText = ref("44.219.64.20")
 function copy() {
-    navigator.clipboard.writeText('44.219.64.20')
+    copyToClipboard('44.219.64.20')
     buttonText.value = "Copied!"
     setTimeout(() => {
         buttonText.value = "44.219.64.20"
     }, 1000)
+}
+
+async function copyToClipboard(textToCopy) {
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+            
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+            
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    }
 }
 </script>
 
