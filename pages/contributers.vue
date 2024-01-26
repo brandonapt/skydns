f28fe35e84a346de281e9f284b3ac8f766bd02c1
brandoge91 <brandoge91@gmail.com>
2024-01-26 11:40:22 -0500

    ok lil bro

---
+++ pages/contributers.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-white font-bold text-center relative top-28">
+      Contributers
+    </h1>
+    <div class="text-center mx-auto w-96 mt-36">
+      <div class="bg-white rounded-lg shadow-lg p-10 text-center border border-purple-500 border-2 hover:text-blue-800 transition">
+        <NuxtLink to="https://github.com/nefarioustechsupport">
+        <h1 class="text-2xl font-bold">NefariousTechSupport</h1>
+        <h1 class="text-md">Actual Research</h1>
+      </NuxtLink>
+      </div>
+      <div class="bg-white rounded-lg shadow-lg mt-5 p-10 text-center border border-pink-500 border-2 hover:text-blue-800 transition">
+        <NuxtLink to="https://github.com/brandonapt">
+        <h1 class="text-2xl font-bold">brandonapt</h1>
+        <h1 class="text-md">DNS, Website, and Hosting</h1>
+      </NuxtLink>
+      </div>
+      <div class="bg-white rounded-lg shadow-lg mt-5 p-10 text-center border border-pink-500 border-2 hover:text-blue-800 transition">
+        <h1 class="text-2xl font-bold">cactusos</h1>
+        <h1 class="text-md">Providing the files</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,76 @@
 <script setup>
-const buttonText = ref("44.219.64.20")
+const buttonText = ref("44.219.64.20");
 function copy() {
-    copyToClipboard('44.219.64.20')
-    buttonText.value = "Copied!"
-    setTimeout(() => {
-        buttonText.value = "44.219.64.20"
-    }, 1000)
+  copyToClipboard("44.219.64.20");
+  buttonText.value = "Copied!";
+  setTimeout(() => {
+    buttonText.value = "44.219.64.20";
+  }, 1000);
 }
 
 async function copyToClipboard(textToCopy) {
-    // Navigator clipboard api needs a secure context (https)
-    if (navigator.clipboard && window.isSecureContext) {
-        await navigator.clipboard.writeText(textToCopy);
-    } else {
-        // Use the 'out of viewport hidden text area' trick
-        const textArea = document.createElement("textarea");
-        textArea.value = textToCopy;
-            
-        // Move textarea out of the viewport so it's not visible
-        textArea.style.position = "absolute";
-        textArea.style.left = "-999999px";
-            
-        document.body.prepend(textArea);
-        textArea.select();
+  // Navigator clipboard api needs a secure context (https)
+  if (navigator.clipboard && window.isSecureContext) {
+    await navigator.clipboard.writeText(textToCopy);
+  } else {
+    // Use the 'out of viewport hidden text area' trick
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
 
-        try {
-            document.execCommand('copy');
-        } catch (error) {
-            console.error(error);
-        } finally {
-            textArea.remove();
-        }
+    // Move textarea out of the viewport so it's not visible
+    textArea.style.position = "absolute";
+    textArea.style.left = "-999999px";
+
+    document.body.prepend(textArea);
+    textArea.select();
+
+    try {
+      document.execCommand("copy");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      textArea.remove();
     }
+  }
 }
 </script>
 
 <template>
-    <div class="relative text-center top-1/3">
-        <h1 class="text-white font-bold text-7xl">SKYDNS</h1>
-        <h3 class="text-white font-semibold text-xl">Bringing Skylanders mobile back</h3>
-        <div class="flex justify-center mt-10">
-            <a @click="copy" target="_blank" class="cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                {{ buttonText }}
-                <Icon name="material-symbols:content-copy-outline" size="24" class="mt-n2" color="black" />
-            </a>
-            </div>
+  <div class="relative text-center top-1/3 left-0 right-0">
+    <h1 class="text-white font-bold text-7xl">SKYDNS</h1>
+    <h3 class="text-white font-semibold text-xl">
+      Bringing Skylanders mobile back
+    </h3>
+    <div class="flex justify-center mt-10">
+      <a
+        @click="copy"
+        target="_blank"
+        class="cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      >
+        {{ buttonText }}
+        <Icon
+          name="material-symbols:content-copy-outline"
+          size="24"
+          class="mt-n2"
+          color="black"
+        />
+      </a>
     </div>
+  </div>
+  <div class="mx-auto text-center absolute top-[50rem] left-0 right-0">
+    <NuxtLink to="/contributers">
+      <a
+        target="_blank"
+        class="cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      >
+        Contributers
+        <Icon
+          name="material-symbols:favorite-outline"
+          size="24"
+          class="mt-n2"
+          color="black"
+        />
+      </a>
+    </NuxtLink>
+  </div>
 </template>
